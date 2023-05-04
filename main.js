b6dcc6982f2a3eeb3239e0a02e34425c1870a625
MarcQueral <mqueral@iesebre.com>
2023-05-04 20:40:37 +0200

    Video 9 acabat

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Pilotes',
-            image: './assets/images/pilota_basquet.jpg',
-            inStock: true,
-            details: ['50% roba', '30% goma','20% aire'],
-            variants: [
-                {id: 2234, color: 'Taronja'},
-                {id: 2235, color: 'Negre'},
-            ]
+            cart: 0,
+            premium: false
         }
-    }
+    },
+    methods: {}
+        
 })
