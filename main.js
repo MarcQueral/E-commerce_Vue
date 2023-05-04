b0b40185f6ea0affd5452898ad33de45a33fbb14
MarcQueral <mqueral@iesebre.com>
2023-05-04 19:29:08 +0200

    Afegir imatge i també diferents característiques (video 5 OK)

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Pilotes'
+            product: 'Pilotes',
+            image: './assets/images/pilota_basquet.jpg',
+            inStock: true,
+            details: ['50% roba', '30% goma','20% aire'],
+            variants: [
+                {id: 2234, color: 'Taronja'},
+                {id: 2235, color: 'Negre'},
+            ]
         }
     }
 })
